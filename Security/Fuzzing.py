010ceeaa938d23eac08733eab709ff59013e0f71
HelloWorld20240418 <minli@netitest.com>
2025-05-22 17:33:12 +0800

    5.22变更

--- Security/Fuzzing.py
+++ Security/Fuzzing.py
@@ -6,14 +6,26 @@ project1.Login("admin", "admin")
 
 case1 = project1.CreateCase("AdvancedFuzzing", "Gateway")
 # case1 = project1.CreateCase("HttpCps", "Client")
-
 case1.Config("Interface", "port1", "port2")
 case1.Config("NetworkSubnet", {"port1": {"SubnetNumber": 1, "IpAddrRange": "18.1.2.2", "Netmask": "16", "ServerIPRange":"18.1.1.2"}})
 case1.Config("NetworkSubnet", {"port2": {"SubnetNumber": 1, "IpAddrRange": "18.1.1.2", "Netmask": "16"}})
 
 
+# test_type: AdvancedFuzzing
+case1.Config("IndexStart", 1)
+case1.Config("IndexEnd", "")
+case1.Config("FuzzDbKeepOnlyNPassCases", 0)
+case1.Config("ReuseTargetConnection", "yes")
+case1.Config("Fuzzing", "UDP协议模糊测试模板")
+case1.Config("ServerPort", 6001)
+#UDP协议模糊测试模板,AMS协议模糊测试模版
+case1.Config("TestDuration", 99999)
+
+
 case1.Apply(case1.case_config)
 case1.Start()
 case1.Monitor()
+case1.AnalysisResult()
 case1.Getresult()
+#case1.DownLoadLogFile(filepath="F:\\FuzzingLog")
 
